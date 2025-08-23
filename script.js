5080393e9532368633a6792c91aefff3f7f2efea
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-23 11:54:50 -0500

    add score to waterbottle page

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ setProperty("ptag", "background-color", "lightblue");
 setProperty("ptag", "padding", "10px");
 
 onEvent("spanishBtn", "click", function() {
+    console.log("Spanish button clicked");
     setText("WaterAppTitle", "Consejos de Conservación de Agua");
     setText("ptag", "Es importante que todos hagamos nuestra parte para usar menos");
 });
 
 onEvent("englishBtn", "click", function() {
+    console.log("English button clicked");
     setText("WaterAppTitle", "Water Conservation Tips");
     setText("ptag", "It's important that we all do our part to use less water.");
 });
--- waterbottle.js
+++ waterbottle.js
@@ -1,9 +1,12 @@
+var score = 0;
 onEvent("gBottle", "click", function() {
     console.log("Glass bottle clicked");
     const gBottle = document.getElementById('gBottle');
     gBottle.style.position = 'absolute'; // Or 'relative', 'fixed'
     gBottle.style.left = '150px';
     gBottle.style.top = '700px';
+    score = score + 1;
+    setText("scoreLabel", score);
 });
 
 onEvent("faucet", "click", function() {
@@ -16,5 +19,13 @@ onEvent("faucet", "click", function() {
 onEvent("pBottle", "click", function() {
     console.log("Plastic bottle clicked");
     setImageURL("pBottle","pBottleno.png" )
-    
+    score = score - 1;
+    setText("scoreLabel", score);
 });
+
+setProperty("Score", "font-size", "30px");
+setProperty("Score", "font-weight", "bold");
+setProperty("Score", "color", "blue");
+setProperty("scoreLabel", "font-size", "24px");
+setProperty("scoreLabel", "font-weight", "bold");
+setProperty("scoreLabel", "color", "orange");
