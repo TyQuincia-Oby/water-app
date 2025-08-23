41a4d1cc7baf1cef8b2f092aeb9a8a368518d049
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-08-22 19:03:42 -0500

    added on events to change languages

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 setProperty("WaterAppTitle", "color", "navy");
+setProperty("ptag", "background-color", "lightblue")
+
+onEvent("spanishBtn", "click", function() {
+    setText("WaterAppTitle", "Consejos de Conservación de Agua");
+    setText("ptag", "Es importante que todos hagamos nuestra parte para usar menos");
+});
+
+onEvent("englishBtn", "click", function() {
+    setText("WaterAppTitle", "Water Conservation Tips");
+    setText("ptag", "It's important that we all do our part to use less water.");
+});
